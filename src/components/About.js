0c8184476a9d5b88ec4901c11ed5ece5ce5f6250
SRF0x41 <sergio.rodriguezflores26@gmail.com>
2025-03-20 00:43:31 -0600

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='About'>
+      <h1>This is the About Page</h1>
+      <p>Here you can learn more about S.R Painting Solutions.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className='Contact'>
+      <h1>Contact Us</h1>
+      <p>Feel free to reach us at contact@srpaintingsolutions.com</p>
+    </div>
+  );
+}
+
+export default Contact;
